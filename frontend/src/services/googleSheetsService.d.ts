f8850fd9ea141434e339283a275b36855c06f648
mickey <mickeywu520@gmail.com>
2025-07-22 01:44:44 +0800

    keeping fixed depoly issue

--- frontend/src/services/googleSheetsService.d.ts
+++ frontend/src/services/googleSheetsService.d.ts
@@ -1,7 +1,7 @@
 declare const googleSheetsService: {
   getStudents: () => Promise<any[]>;
   getCourses: () => Promise<any[]>;
-  addMakeupRequest: (data: any) => Promise<any>;
+  addMakeupRequest: (studentId: string, originalCourseId: string, makeupCourseId: string, reason: string) => Promise<any>;
   [key: string]: any;
 };
 
